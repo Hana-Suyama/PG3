650c9586cb7af82fb600aac7f9240bee6e162a27
Hana-Suyama <115055069+Hana-Suyama@users.noreply.github.com>
2023-10-16 12:11:16 +0900

    01_01

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
+template <typename Type>
+Type min(Type a, Type b) {
+	if (a > b) {
+		return static_cast<Type>(b);
+	} else {
+		return static_cast<Type>(a);
+	}
+}
+
+template <>
+char min<char>(char a, char b) {
+	printf("”šˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ\n");
+	return 0;
+}
+
 int main(){
 
+	min<int>(5, 3);
+	min<float>(5.0f, 3.0f);
+	min<double>(5.0f, 3.0f);
+	min<char>('4', '2');
 
 	return 0;
 }
